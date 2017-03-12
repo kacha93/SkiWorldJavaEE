ebad442432cb6207e35c19dd928cf11f1a6aad33
kacha93 <ahmed.kachabia93@gmail.com>
2017-03-12 02:46:12 +0100

    Commit Ahmed

--- SkiWorld-ejb/src/main/java/tn/esprit/entities/User.java
+++ SkiWorld-ejb/src/main/java/tn/esprit/entities/User.java
@@ -1,7 +1,11 @@
 package tn.esprit.entities;
 
 import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.xml.bind.annotation.XmlRegistry;
 
 /**
  * Entity implementation class for Entity: User
@@ -13,12 +17,21 @@ public class User implements Serializable {
 
 	
 	private int id;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	private String nationalId;
+	private String email;
+	private Adress adress;
+	private String password ; 
+	private String login;
 	private static final long serialVersionUID = 1L;
 
 	public User() {
 		super();
 	}   
 	@Id    
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	public int getId() {
 		return this.id;
 	}
@@ -26,5 +39,61 @@ public class User implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Temporal(TemporalType.DATE)
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getNationalId() {
+		return nationalId;
+	}
+	public void setNationalId(String nationalId) {
+		this.nationalId = nationalId;
+	}
+	
+	@Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Embedded
+	public Adress getAdress() {
+		return adress;
+	}
+	public void setAdress(Adress adress) {
+		this.adress = adress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
    
 }
