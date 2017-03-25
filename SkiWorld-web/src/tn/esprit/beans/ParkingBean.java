b0a2682d959ff6b9c59eeb0e39fc47a08a937823
dali <ali.ghaffari@esprit.tn>
2017-03-26 00:19:19 +0100

    test

--- SkiWorld-web/src/tn/esprit/beans/ParkingBean.java
+++ SkiWorld-web/src/tn/esprit/beans/ParkingBean.java
@@ -2,10 +2,12 @@ package tn.esprit.beans;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import tn.esprit.entities.Hotel;
 import tn.esprit.entities.Parking;
 import tn.esprit.services.ParkingServiceLocal;
 
@@ -17,14 +19,51 @@ public class ParkingBean {
 
 	public ParkingBean() {
 	}
+	
 	@EJB
 	ParkingServiceLocal ejb;
 	
-	Parking item = new Parking();
-	List<Parking> items=ejb.findAll() ;
+	private Parking item = new Parking();
+	private List<Parking> items ;
+	private boolean isEditVisible =false; 
+	private boolean isAddVisible = false;
+	
+	
+	@PostConstruct
+	public void init(){
+		item = new Parking();
+		item.setHotel(new Hotel());
+		items = ejb.findAll();
+	}
+	
+public void prepareCreate(){
+		
+		
+		this.setAddVisible(true);
+		init();
+	}
 	
 	public void create(){
 		ejb.create(item);
+	}
+	
+	public void prepareEdit(Parking park){
+		item = park;
+		setEditVisible(true);
+		
+	}
+	
+	public void edit(){
+		ejb.edit(item);
+		this.setEditVisible(false);
+		init();
+	}
+	
+	public void delete (Parking park){
+		
+		ejb.delete(park);
+		init();
+		
 	}
 	
 	public List<Parking> getItems (){
@@ -46,6 +85,25 @@ public class ParkingBean {
 		this.items = items;
 	}
 	
+	
+	public boolean isEditVisible() {
+		return isEditVisible;
+	}
+
+
+	public void setEditVisible(boolean isEditVisible) {
+		this.isEditVisible = isEditVisible;
+	}
+
+
+	public boolean isAddVisible() {
+		return isAddVisible;
+	}
+
+
+	public void setAddVisible(boolean isAddVisible) {
+		this.isAddVisible = isAddVisible;
+	}
 	
 
 }
